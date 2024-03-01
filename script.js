6e2eff4c1d8bca6604562686389fe7fd65358878
hsnaregia <1392811016+hsnaregia@user.noreply.github.com>
2024-03-02 00:16:11 +0330

    todo list website

---
+++ script.js
@@ -0,0 +1,22 @@
+const root = document.documentElement;
+// Get the toggle button
+const toggle = document.getElementById("toggle");
+// Get the user's preference from localStorage
+const darkMode = localStorage.getItem("dark-mode");
+// Check if the user has already chosen a theme
+if (darkMode) {
+  // If yes, apply it to the root element
+  root.classList.add("dark-theme");
+}
+// Add an event listener to the toggle button
+toggle.addEventListener("click", () => {
+  // Toggle the dark-theme class on the root element
+  root.classList.toggle("dark-theme");
+  // Store or remove the user's preference in localStorage
+  if (root.classList.contains("dark-theme")) {
+    localStorage.setItem("dark-mode", true);
+  } else {
+    localStorage.removeItem("dark-mode");
+  }
+});
+
